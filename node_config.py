6e444d52b59db86e966825f0e5ae68cf186d0781
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-02-27 10:16:13 -0600

    remove pint

--- node_config.py
+++ node_config.py
@@ -1,7 +1,3 @@
-import pint
-
-ur = pint.UnitRegistry()
-
 # Define possible node types
 NODE_TYPE_SIMULATED = 0
 NODE_TYPE_PRIMARY = 1
@@ -14,7 +10,8 @@ node_type = 0
 # Total number of zones in the system
 num_zones = 3
 
-iphone = 5.94 * ur.inch
+inch_to_meter = 1 / 39.37
+iphone = 5.94 * inch_to_meter
 
 # Volumes of the model rooms are measured in iphone 11s and inches because we
 # didn't have any calculators haha.
@@ -24,7 +21,7 @@ room_a_height = room_b_height = 1 * iphone
 
 room_c_width = 1 * iphone
 room_c_height = 2 * iphone
-room_c_length = 4 * iphone + 2 * ur.inch
+room_c_length = 4 * iphone + 2 * inch_to_meter
 
 volume_a = room_a_width * room_a_height * room_a_length
 volume_b = room_b_width * room_b_height * room_b_length
@@ -38,15 +35,15 @@ area_c = (
     + room_c_width * room_c_length
 )
 
-U = 2.6 * ur.W / (ur.m**2 * ur.K)
-rho = 1.225 * ur.kg / (ur.m**3)
-c_p = 1005 * ur.J / (ur.kg * ur.K)
+U = 2.6
+rho = 1.225
+c_p = 1005
 
 # the units work out, see comments in simulation._update_temps
 zone_k = {
-    0: ((U * area_a) / (rho * volume_a * c_p)).to_base_units().magnitude,
-    1: ((U * area_b) / (rho * volume_b * c_p)).to_base_units().magnitude,
-    2: ((U * area_c) / (rho * volume_c * c_p)).to_base_units().magnitude,
+    0: (U * area_a) / (rho * volume_a * c_p),
+    1: (U * area_b) / (rho * volume_b * c_p),
+    2: (U * area_c) / (rho * volume_c * c_p),
 }
 
 zone_names = ["A", "B", "C"]
